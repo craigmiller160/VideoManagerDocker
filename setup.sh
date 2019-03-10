d9fe878b62e2e702dadc271b59fd1ec9f34e7c4c
Craig Miller <craigmiller160@gmail.com>
2019-03-10 13:39:53 -0400

    Cleaned up some of the setup and docker code

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Setup script for video manager envrionment
 
+echo "Creating postgres docker volume"
 mkdir -p ~/.video-manager/postgres
+echo "Creating pgadmin docker volume"
 mkdir -p ~/.video-manager/pgadmin
