75fd96ad0a491c614b6c96bb62db1db0f8b982fa
Craig Miller <craigmiller160@gmail.com>
2019-04-11 16:49:19 -0400

    Added script to run a docker version of vlc

---
+++ vlc-docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker run \
+	-v $HOME/Documents:/home/vlc/Documents:rw \
+	-v /dev/snd:/dev/snd --privileged \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	-e uid=$(id -u) \
+	-e gid=$(id -g) \
+	-e DISPLAY=unix$DISPLAY \
+	--name vlc \
+	chrisdaish/vlc
